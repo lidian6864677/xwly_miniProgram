a426c9d89fe62d465e7c0d89dfd98ac7675f169c
Dian <lidian@tuhu.cn>
2019-06-13 17:57:30 +0800

    login 登录样式

--- pages/member/index/index.js
+++ pages/member/index/index.js
@@ -17,7 +17,7 @@ Page({
 	onShow: function () {
 		var that = this;
 		// 获得当前登录用户
-		const user = AV.User.current();
+		// const user = AV.User.current();
 		// 调用小程序 API，得到用户信息
     // this.setData({
       // userInfo: app.globalData.userInfo
@@ -35,33 +35,33 @@ Page({
 		// 	}
 		// });
 	},
-	chooseImage: function () {
-		var that = this;
-		wx.chooseImage({
-			count: 1, // 默认9
-			sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-			sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-			success: function (res) {
-				// 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-				var tempFilePath = res.tempFilePaths[0];
-				new AV.File('file-name', {
-					blob: {
-						uri: tempFilePath,
-					},
-				}).save().then(
-				// file => console.log(file.url())
-					function(file) {
-						// 上传成功后，将所上传的头像设置更新到页面<image>中
-						var userInfo = that.data.userInfo;
-						userInfo.avatarUrl = file.url();
-						that.setData({
-							userInfo, userInfo
-						});
-					}
-				).catch(console.error);
-			}
-		})
-	},
+	// chooseImage: function () {
+	// 	var that = this;
+	// 	wx.chooseImage({
+	// 		count: 1, // 默认9
+	// 		sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+	// 		sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+	// 		success: function (res) {
+	// 			// 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+	// 			var tempFilePath = res.tempFilePaths[0];
+	// 			new AV.File('file-name', {
+	// 				blob: {
+	// 					uri: tempFilePath,
+	// 				},
+	// 			}).save().then(
+	// 			// file => console.log(file.url())
+	// 				function(file) {
+	// 					// 上传成功后，将所上传的头像设置更新到页面<image>中
+	// 					var userInfo = that.data.userInfo;
+	// 					userInfo.avatarUrl = file.url();
+	// 					that.setData({
+	// 						userInfo, userInfo
+	// 					});
+	// 				}
+	// 			).catch(console.error);
+	// 		}
+	// 	})
+	// },
 	navigateToAboutus: function () {
 		wx.navigateTo({
 			url: '/pages/member/aboutus/aboutus'
